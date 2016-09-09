c4704464f4849aa3a0a5c0b8ae24876dab3673d2
damcl@hempel.com <damcl@hempel.com>
2016-09-09 08:52:53 +0200

    parseEcoInsightXLS: loop over input files, output struct, update documentation

--- parseEcoInsightXLS.m
+++ parseEcoInsightXLS.m
@@ -1,32 +1,56 @@
-function [ dates, pidx, regr, dataStartRow, ship] = parseEcoInsightXLS( filename )
+function [timedata, dataStartRow, ship] = parseEcoInsightXLS( filename )
 %parseEcoInsightXLS Parse relevant data from EcoInsight time-series data
 %   [dates, pidx, regr, dataStartRow, ship] = parseEcoInsightXLS(FILENAME)
 %   will return from input FILENAME, a string giving the full path to a
 %   .xlsx file downloaded from DNVGL's EcoInsight web graphical interface,
-%   the values for the dates in the vector DATES, the corresponding 
-%   performance index values in PIDX, the corresponding regression line 
-%   values in REGR, the first row of the data in the file in scalar 
-%   DATASTARTROW and all non-time-dependent data in struct SHIP.
+%   the values for the dates, the corresponding performance index values 
+%   and the corresponding regression line values in struct TIMEDATA, the 
+%   first row of the data in the file in scalar DATASTARTROW and all 
+%   non-time-dependent data in struct SHIP. TIMEDATA will have the fields 
+%   DATES, PIDX and REGR containing the above-mentioned data.
 
-% Read
-[dat, txt] = xlsread(filename);
-dataStartRow = find(cellfun(@(x) isequal(x, 'Date'), txt(:, 1)), 1) + 1;
-date_c = txt(dataStartRow:end, 1);
-pidx = dat(:, 1);
-regr = dat(:, 3);
+% Inputs
+validateattributes(filename, {'char', 'cell'}, {'vector'}, ...
+    'parseEcoInsightXLS', 'filename', 1);
+filename = cellstr(filename);
 
-shipDataLength = 18;
-shipdataEnd = dataStartRow - 3;
-shipdataStart = shipdataEnd - shipDataLength;
-shipdata = txt(shipdataStart : shipdataEnd, [1, 4]);
-shipdata( cellfun(@isempty, shipdata(:, 1)), :) = [];
-fnames_c = shipdata(:, 1);
-fnames_c = regexprep(fnames_c, {' ', '[', '%', ']'}, repmat({'_'}, 1, 4));
-ship = cell2struct(shipdata(:, 2), fnames_c, 1);
+% Initalise Outputs
+numFiles = numel(filename);
+timedata = struct('dates', [], 'pidx', [], 'regr', []);
+% ship = repmat(struct(), [numFiles, 1]);
+dataStartRow = nan(1, numFiles);
 
-% Convert Date
-date_c = cellfun(@(x) datenum(x, 'dd-mm-yyyy'), date_c, 'Uni', 0);
-dates = [date_c{:}];
-dates = dates(:);
-
+for fi = 1:numFiles
+    
+    % Read
+    [dat, txt] = xlsread(filename{fi});
+    dataStartRow(fi) = find(cellfun(@(x) isequal(x, 'Date'), txt(:, 1)), 1) + 1;
+    date_c = txt(dataStartRow(fi):end, 1);
+    pidx = dat(:, 1);
+    regr = dat(:, 3);
+    
+    shipDataLength = 18;
+    shipdataEnd = dataStartRow(fi) - 3;
+    shipdataStart = shipdataEnd - shipDataLength;
+    shipdata = txt(shipdataStart : shipdataEnd, [1, 4]);
+    shipdata( cellfun(@isempty, shipdata(:, 1)), :) = [];
+    fnames_c = shipdata(:, 1);
+    fnames_c = regexprep(fnames_c, {' ', '[', '%', ']'}, repmat({'_'}, 1, 4));
+    
+    if fi == 1
+        ship(fi) = cell2struct(cell(length(fnames_c), 1), fnames_c, 1);
+        % ship = repmat(struct(), [numFiles, 1]);
+    end
+    
+    ship(fi) = cell2struct(shipdata(:, 2), fnames_c, 1);
+    
+    % Convert Date
+    date_c = cellfun(@(x) datenum(x, 'dd-mm-yyyy'), date_c, 'Uni', 0);
+    dates = [date_c{:}];
+    dates = dates(:);
+    
+    timedata(fi).dates = dates;
+    timedata(fi).pidx = pidx;
+    timedata(fi).regr = regr;
+    
 end
