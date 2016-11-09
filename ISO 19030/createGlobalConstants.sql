a0f610217b4a0f6511c9f6aa1e2db1e41c662c27
damcl@hempel.com <damcl@hempel.com>
2016-11-09 15:16:02 +0100

    create table scripts updated to include new columns

--- ISO 19030/createGlobalConstants.sql
+++ ISO 19030/createGlobalConstants.sql
@@ -1,4 +1,5 @@
 /* Create table for global constants */
 
-CREATE TABLE globalConstants (id INT PRIMARY KEY AUTO_INCREMENT, Specific_Gas_Constant_Air DOUBLE(6, 3));
-INSERT INTO globalConstants (Specific_Gas_Constant_Air) VALUES (287.058);
+DROP TABLE IF EXISTS globalConstants;
+CREATE TABLE globalConstants (id INT PRIMARY KEY AUTO_INCREMENT, Specific_Gas_Constant_Air DOUBLE(6, 3), g DOUBLE(10, 5));
+INSERT INTO globalConstants (Specific_Gas_Constant_Air, g) VALUES (287.058, 9.80665);
--- ISO 19030/createSFOCCoefficients.sql
+++ ISO 19030/createSFOCCoefficients.sql
@@ -1,3 +1,3 @@
 /* Create table for coefficients of SFOC fitted curve */
 
-CREATE TABLE SFOCCoefficients (id INT PRIMARY KEY AUTO_INCREMENT, Engine_Model VARCHAR(100), X0 DOUBLE(20, 10), X1 DOUBLE(20, 10),  X2 DOUBLE(20, 10));
+CREATE TABLE SFOCCoefficients (id INT PRIMARY KEY AUTO_INCREMENT, Engine_Model VARCHAR(100), X0 DOUBLE(20, 10), X1 DOUBLE(20, 10),  X2 DOUBLE(20, 10), Minimum_FOC_ph DOUBLE(10, 3));
