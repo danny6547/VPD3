18a3cd5267855a60ce918342a16968a9f417ce43
damcl@hempel.com <damcl@hempel.com>
2016-11-11 08:49:15 +0100

    Add documentation of EcoInsight downloaded data.

--- LoadInfilePerformanceData.sql
+++ LoadInfilePerformanceData.sql
@@ -1,3 +1,3 @@
 /* Script to load data into table "Performance Data" from tab-delimited text file with Performance Index, Time and Ship Name columns */
 USE test2;
-LOAD DATA LOCAL INFILE 'C://Users//damcl//Documents//Ship Data//test//AllButCMAYM.tab' INTO TABLE `Performance Data` IGNORE 0 LINES (`Performance Index`, Date, IMO);
+LOAD DATA LOCAL INFILE 'C://Users//damcl//Documents//Ship Data//CMA CGM//Almaviva//SpeedIndex.tab' INTO TABLE `Performance Data` IGNORE 0 LINES (`Speed Index`, Date, IMO);
