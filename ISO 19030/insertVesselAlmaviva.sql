404d58c38643001a81bc81ba5dfb0d89d65c2542
damcl@hempel.com <damcl@hempel.com>
2016-11-15 13:23:24 +0100

    vessels: column "Speed Power Source", where data was sourced from.

--- ISO 19030/insertVesselAlmaviva.sql
+++ ISO 19030/insertVesselAlmaviva.sql
@@ -2,5 +2,5 @@
 
 use test2;
 
-INSERT INTO Vessels (IMO_Vessel_Number, Name, 				Owner, 		Engine_Model, Wind_Resist_Coeff_Head, Wind_Resist_Coeff_Dir, Transverse_Projected_Area_Design, Block_Coefficient, Length_Overall, Breadth_Moulded, Draft_Design, LBP) 
-			 VALUES (9450648, 		    'CMA CGM Almaviva', 'CMA CGM', '12k98me-c7',  NULL, 				  NULL, 				 1330, 							   0.62, 			  334, 			  42.8, 		   15, 			 319);
+INSERT INTO Vessels (IMO_Vessel_Number, Name, 				Owner, 		Engine_Model, Wind_Resist_Coeff_Head, Wind_Resist_Coeff_Dir, Transverse_Projected_Area_Design, Block_Coefficient, Length_Overall, Breadth_Moulded, Draft_Design, LBP, Speed_Power_Source)
+			 VALUES (9450648, 		    'CMA CGM Almaviva', 'CMA CGM', '12k98me-c7',  NULL, 				  NULL, 				 1330, 							   0.62, 			  334, 			  42.8, 		   15, 			 319, '\\hempel.net\Function\RD\Group\Marine Business Support\Hull and propeller performance\Vessels\CMA CGM\Hempel Collaboration\model tests\Model Tests 8500 SHI\Calm Water Model Tests for HN1787s - 8500TEU Container Carrier\SFRMRSHOP1216012914290.pdf');
