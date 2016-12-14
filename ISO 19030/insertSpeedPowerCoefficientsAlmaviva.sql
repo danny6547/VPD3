49ff83675d14c85ecefa8c4a1798097fd3c700d7
damcl@hempel.com <damcl@hempel.com>
2016-12-14 14:25:10 +0100

    insertSpeedPowerCoefficientsAlmaviva: include R Squared

--- ISO 19030/insertSpeedPowerCoefficientsAlmaviva.sql
+++ ISO 19030/insertSpeedPowerCoefficientsAlmaviva.sql
@@ -1,7 +1,8 @@
 /* Insert Speed Power Coefficients for Vessel Almaviva */
 
-INSERT INTO speedPowerCoefficients (IMO_Vessel_Number, Displacement, Trim, 		   Exponent_A, 			Exponent_B)
-							VALUES (9450648, 				 114050, 0,     6.037644473511698, -40.659732310548080),
-								   (9450648, 				  48800, 3.3-9.56,  5.037644473511698, -41.659732310548080),
-								   (9450648, 				 137090, 0,     4.037644473511698, -42.659732310548080)
+INSERT INTO speedPowerCoefficients (IMO_Vessel_Number, Displacement, Trim, 		   Exponent_A, 			Exponent_B, 		R_Squared)
+							VALUES (9450648, 				  48800, 3.3-9.56,  5.037644473511698, -41.659732310548080, 		NULL),
+								   (9450648, 				 137090, 0,     	4.037644473511698, -42.659732310548080, 		NULL)
 																													;
+                                                                                                                    
+/* 9450648, 				 114050, 0,     6.037644473511698, -40.659732310548080 */
