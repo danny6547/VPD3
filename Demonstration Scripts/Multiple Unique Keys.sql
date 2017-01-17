a4eecd61d3a8b4fdf6743a9b7c75f97541119183
damcl@hempel.com <damcl@hempel.com>
2017-01-17 15:11:48 +0100

    Add demonstration of unique key updates

---
+++ Demonstration Scripts/Multiple Unique Keys.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS temp1;
+CREATE TABLE temp1 (id INT PRIMARY KEY, a INT, b INT, c INT, d INT, e INT, CONSTRAINT ida UNIQUE(a, b));
+INSERT INTO temp1 (id, a, b) VALUES (1, 2, 0);
+INSERT INTO temp1 (id, a, b) VALUES (2, 2, 1);
+INSERT INTO temp1 (id, a, b, c) VALUES (3, 2, 2, 0);
+INSERT INTO temp1 (id, a, b, c, d) VALUES (3, 2, 2, 4, 5) ON duplicate key update c = values(c), d = values(d);
+INSERT INTO temp1 (id, a, b, c, d) VALUES (3, 2, 2, 4, 4) ON duplicate key update c = values(c), d = values(d);
+SELECT (SELECT COUNT(e) FROM temp1) != 0;
