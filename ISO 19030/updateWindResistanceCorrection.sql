d12a4c0e86bdf3f0425142e040616d511ac293d5
damcl@hempel.com <damcl@hempel.com>
2017-01-17 15:42:14 +0100

    updateWindResistanceCorrection: fix bug where incorrect output detected in test

--- ISO 19030/updateWindResistanceCorrection.sql
+++ ISO 19030/updateWindResistanceCorrection.sql
@@ -12,7 +12,7 @@ BEGIN
 		((Wind_Resistance_Relative - Air_Resistance_No_Wind) * Speed_Over_Ground / (SELECT Propulsive_Efficiency FROM SpeedPower WHERE IMO_Vessel_Number = imo)) + 
 		Delivered_Power * (1 - (0.7 / (SELECT Propulsive_Efficiency FROM SpeedPower WHERE IMO_Vessel_Number = imo)));
 	*/
-	
+    
 	UPDATE tempRawISO e 
 		JOIN
 			(SELECT q.id, w.Propulsive_Efficiency, w.Speed, w.Power, q.Delivered_Power FROM tempRawISO q
@@ -20,12 +20,11 @@ BEGIN
 					ON q.IMO_Vessel_Number = w.IMO_Vessel_Number AND
 						q.NearestDisplacement = w.Displacement AND
 						q.NearestTrim = w.Trim
-				GROUP BY Delivered_Power) r
+				GROUP BY id) r
 		ON e.id = r.id
 		SET e.Wind_Resistance_Correction = (
 			(e.Wind_Resistance_Relative - e.Air_Resistance_No_Wind) * e.Speed_Over_Ground / r.Propulsive_Efficiency) + 
 			e.Delivered_Power * (1 - (0.7 / r.Propulsive_Efficiency)
 			)
 			;
-	
 END;
