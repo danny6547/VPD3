734b8396bbb9c01ba5cb56dde71819af02087efa
damcl@hempel.com <damcl@hempel.com>
2017-01-17 15:39:38 +0100

    updateCorrectedPower: apply wind correction if available, declare compliant

--- ISO 19030/updateCorrectedPower.sql
+++ ISO 19030/updateCorrectedPower.sql
@@ -1,11 +1,23 @@
 /* Correct delivered power values for environmental factors */
+DROP PROCEDURE IF EXISTS updateCorrectedPower;
 
 delimiter //
 
 CREATE PROCEDURE updateCorrectedPower()
 BEGIN
-	
-	UPDATE tempRawISO
-	SET Corrected_Power = Delivered_Power - Wind_Resistance_Correction;
-	
+    
+    DECLARE WindResValid BOOLEAN;
+    SET WindResValid := (SELECT COUNT(Wind_Resistance_Correction) FROM tempRawISO) != 0;
+    
+    IF WindResValid THEN
+		UPDATE tempRawISO SET Corrected_Power = Delivered_Power - Wind_Resistance_Correction;
+    ELSE
+		UPDATE tempRawISO SET Corrected_Power = Delivered_Power;
+	END IF;
+    
+    IF @imo IS NOT NULL AND @startd IS NOT NULL AND @endd IS NOT NULL THEN
+		CALL IMOStartEnd(@imo, @startd, @endd);
+		INSERT INTO StandardCompliance (IMO_Vessel_Number, StartDate, EndDate, WindResistanceApplied) 
+		VALUES (@imo, @startd, @endd, WindResValid) ON duplicate key update WindResistanceApplied = values(WindResistanceApplied);
+    END IF;
 END;
