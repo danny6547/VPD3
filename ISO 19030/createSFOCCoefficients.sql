8fb7adfbf4a87ce4b9da85e4cefc547985b3314b
damcl@hempel.com <damcl@hempel.com>
2017-01-17 15:29:50 +0100

    createSFOCCoefficients: include extents of brake power

--- ISO 19030/createSFOCCoefficients.sql
+++ ISO 19030/createSFOCCoefficients.sql
@@ -1,3 +1,3 @@
 /* Create table for coefficients of SFOC fitted curve */
 
-CREATE TABLE SFOCCoefficients (id INT PRIMARY KEY AUTO_INCREMENT, Engine_Model VARCHAR(100), X0 DOUBLE(20, 10), X1 DOUBLE(20, 10),  X2 DOUBLE(20, 10), Minimum_FOC_ph DOUBLE(10, 3));
+CREATE TABLE SFOCCoefficients (id INT PRIMARY KEY AUTO_INCREMENT, Engine_Model VARCHAR(100), X0 DOUBLE(20, 10), X1 DOUBLE(20, 10),  X2 DOUBLE(20, 10), Minimum_FOC_ph DOUBLE(10, 3), Lowest_Given_Brake_Power DOUBLE(10, 3), Highest_Given_Brake_Power DOUBLE(10, 3));
