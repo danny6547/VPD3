71dd5b0ae7bf962f14f8e5e3246703bbe4c0eaa4
damcl@hempel.com <damcl@hempel.com>
2017-01-23 09:52:23 +0100

    normaliseHigherFreq: bugfix grouping didn't always start at first row

--- ISO 19030/normaliseHigherFreq.sql
+++ ISO 19030/normaliseHigherFreq.sql
@@ -13,12 +13,14 @@ CREATE PROCEDURE normaliseHigherFreq()
 BEGIN
 
     DECLARE timeStep DOUBLE(20, 3);
+    DECLARE minTime INT8(12);
     
     /* Check for higher-frequency secondary parameters */
     
     /* Get primary parameter timestep */
     SET timeStep := (SELECT (SELECT to_seconds(DateTime_UTC) FROM tempRawISO WHERE Speed_Over_Ground IS NOT NULL LIMIT 1, 1) - 
 		(SELECT to_seconds(DateTime_UTC) FROM tempRawISO WHERE Speed_Over_Ground IS NOT NULL LIMIT 0, 1) );
+	SET minTime = (SELECT TO_SECONDS(MIN(DateTime_UTC)) FROM tempRawISO);
     
     /* Average all secondary parameters to this value */
     DROP TABLE IF EXISTS tempRawISO1;
@@ -27,7 +29,7 @@ BEGIN
     INSERT INTO tempRawISO1 (DateTime_UTC, IMO_Vessel_Number, Relative_Wind_Speed, Relative_Wind_Direction, Speed_Over_Ground, Ship_Heading, Shaft_Revolutions, Static_Draught_Fore, Static_Draught_Aft, Water_Depth, Rudder_Angle, Seawater_Temperature, Air_Temperature, Air_Pressure, Air_Density, Speed_Through_Water, Delivered_Power, Shaft_Power, Brake_Power, Shaft_Torque, Mass_Consumed_Fuel_Oil, Volume_Consumed_Fuel_Oil, Displacement) 
     SELECT (DateTime_UTC), IMO_Vessel_Number, AVG(Relative_Wind_Speed), AVG(Relative_Wind_Direction), AVG(Speed_Over_Ground), AVG(Ship_Heading), AVG(Shaft_Revolutions), AVG(Static_Draught_Fore), AVG(Static_Draught_Aft), AVG(Water_Depth), AVG(Rudder_Angle), AVG(Seawater_Temperature), AVG(Air_Temperature), AVG(Air_Pressure), AVG(Air_Density), AVG(Speed_Through_Water), AVG(Delivered_Power), AVG(Shaft_Power), AVG(Brake_Power), AVG(Shaft_Torque), AVG(Mass_Consumed_Fuel_Oil), AVG(Volume_Consumed_Fuel_Oil), AVG(Displacement)
 		FROM tempRawISO
-			GROUP BY FLOOR(TO_SECONDS(DateTime_UTC)/(timeStep));
+			GROUP BY FLOOR((TO_SECONDS(DateTime_UTC) - minTime) / timeStep);
     
     DROP TABLE tempRawISO;
     RENAME TABLE tempRawISO1 TO tempRawISO;
