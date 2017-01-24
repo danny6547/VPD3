761e4be582600771a784f096fa68e07fcc1930cf
damcl@hempel.com <damcl@hempel.com>
2017-01-24 10:34:28 +0100

    cShipWindCoefficient: adding template class file

---
+++ cShipWindCoefficient/Code/@cShipWindCoefficient/cShipWindCoefficient.m
@@ -0,0 +1,20 @@
+classdef cShipWindCoefficient
+    %CSHIPWINDCOEFFICIENT Wind resistance coefficents for ships.
+    %   Detailed explanation goes here
+    
+    properties
+        
+        Direction double = [];
+        Value double = [];
+        
+    end
+    
+    methods
+    
+       function obj = cShipWindCoefficient()
+    
+       end
+    
+    end
+    
+end
