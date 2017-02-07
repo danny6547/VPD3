948fc600948ad71c1bbd29076933c7b6e3c0b253
damcl@hempel.com <damcl@hempel.com>
2017-02-07 11:55:27 +0100

    add demo for generating meta data from performance data

---
+++ Demonstration Scripts/MetaData.sql
@@ -0,0 +1,11 @@
+/* Example showing extraction of values calculated from performance data, including grouping and ordering */
+
+SELECT Name, v.IMO_Vessel_Number, 
+	COUNT(Performance_Index) AS '# Performance Data',
+    TIMESTAMPDIFF(MONTH, MIN(DateTime_UTC), MAX(DateTime_UTC)) AS 'Data Duration (months)',
+    MIN(DATE(DateTime_UTC)) AS 'Start Date'
+		FROM vessels v
+			JOIN performancedatadnvgl pd
+				ON v.IMO_Vessel_Number = pd.IMO_Vessel_Number
+					GROUP BY IMO_Vessel_Number
+						ORDER BY Name;
