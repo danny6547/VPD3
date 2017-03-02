ea7020d5bc9331bcfa408f21f5f21db6453986fa
damcl@hempel.com <damcl@hempel.com>
2017-03-02 10:11:35 +0100

    cVA.filterOnUniqueIndex: test

--- Vessel Descriptions/cVessel/Test/testcVessel.m
+++ Vessel Descriptions/cVessel/Test/testcVessel.m
@@ -1,5 +1,5 @@
 classdef testcVessel < matlab.unittest.TestCase
-%TESTCSHIP
+%TESTCVESSEL
 %   Detailed explanation goes here
 
 properties
@@ -7,11 +7,50 @@ end
 
 methods(Test)
 
-    function firsttest(testcase)
+    function testfilterOnUniqueIndex(testcase)
+    % testfilterOnUniqueIndex Test that method will remove duplicate values
+    % of the index data and the corresponding elements of the other data.
+    % 1. Test that, when duplicate elements exist for the data in property
+    % given by input UNIQUE, the duplicates will be removed and the
+    % corresponding elements from the properties given by PROPS will also
+    % be deleted.
     
+    % 1
+    % Input
+    inputUnique = 'DateTime_UTC';
+    inputProp = {'Speed_Index', 'Performance_Index'};
+    nonUniDate = repmat(now-1:now+1, [3, 1]);
+    nonUniDate = nonUniDate(:);
+    szData = [9, 1];
+    shipdata = repmat(struct('DateTime_UTC', nonUniDate, 'Speed_Index', ...
+        randn(szData), 'Performance_Index', randn(szData), ...
+        'IMO_Vessel_Number', 1234567), [2, 2]);
+    inputObj = cVessel('ShipData', shipdata, 'Name', 'Example Vessel');
+    expUni = inputObj;
+    actUni = inputObj;
+    [uniqueData, uniI] = unique(nonUniDate);
+    [expUni.(inputUnique)] = deal(uniqueData);
+    
+    for ii = 1:numel(inputProp)
+        
+        inData = expUni.(inputProp{ii});
+        [expUni.(inputProp{ii})] = deal(inData(uniI));
     end
     
+    % Execute
+    actUni = actUni.filterOnUniqueIndex(inputUnique, inputProp);
+    
+    % Verify
+    msgUni = ['Non-unique elements are expected to be removed from data in '...
+        'properties given by UNIQUE and PROPS'];
+    testcase.verifyEqual(actUni, expUni, msgUni);
+    
+    end
 end
 
-end
+% Input
 
+% Execute
+
+% Verify
+end
