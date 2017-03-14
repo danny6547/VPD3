c785cf18e8a5acfc0b8d39a035413ea172143e9e
damcl@hempel.com <damcl@hempel.com>
2017-03-14 12:57:06 +0100

    adding class for dry docking dates

---
+++ Vessel Descriptions/cVesselDryDockDates/@cVesselDryDockDates/cVesselDryDockDates.m
@@ -0,0 +1,21 @@
+classdef cVesselDryDockDates
+    %CVESSELDRYDOCKDATES Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        
+        IMO_Vessel_Number = [];
+        StartDate = [];
+        EndDate = [];
+        
+    end
+    
+    methods
+    
+       function obj = cVesselDryDockDates()
+    
+       end
+    
+    end
+    
+end
