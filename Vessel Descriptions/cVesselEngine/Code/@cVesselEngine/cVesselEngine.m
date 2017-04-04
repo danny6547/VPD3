56a04a286945f97c804b5b5b68f8769d2bfb25af
damcl@hempel.com <damcl@hempel.com>
2017-04-04 12:46:35 +0200

    cVE.fitData2Quadratic: write

--- Vessel Descriptions/cVesselEngine/Code/@cVesselEngine/cVesselEngine.m
+++ Vessel Descriptions/cVesselEngine/Code/@cVesselEngine/cVesselEngine.m
@@ -25,13 +25,37 @@ classdef cVesselEngine < handle
            
        end
        
-       function obj = fitData2Quadratic(obj, mcr, sfoc, perCentMcr)
-       % 
-           
-           
+       function obj = fitData2Quadratic(obj, mcr, sfoc, powerPCT)
+       % fitData2Quadratic Assign coefficients of quadratic fit of data
+       
+       for oi = 1:numel(obj)
+            
+            currObj = obj(oi);
+            
+            % Get FOC, Power 
+            power = currObj.powerFromMCRPCT(powerPCT, mcr);
+            FOC = currObj.FOCFromSFOC(sfoc, power);
+            
+            % Assign vectors
+            currObj.MCR = mcr;
+            currObj.Power = power;
+            currObj.SFOC = sfoc;
+            
+            % Assign limits
+            obj.limitsOfData;
+            
+            % Fit Data
+            fit_v = polyfit(FOC, power, 2);
+            
+            % Assign coefficients
+            currObj.X0 = fit_v(end);
+            currObj.X1 = fit_v(end-1);
+            currObj.X2 = fit_v(end-2);
+            
+       end
        end
        
-       function [LowestFOCph, LowestPower, HighestPower] = limitsOfData(obj)
+       function [obj, LowestFOCph, LowestPower, HighestPower] = limitsOfData(obj)
        % limitsOfData Find limits of power, fuel consumption data
        
        numObj = numel(obj);
@@ -42,16 +66,39 @@ classdef cVesselEngine < handle
        for oi = 1:numObj
            
            if ~isempty(obj(oi).Power) && ~isempty(obj(oi).MCR)
-               currPower = (obj(oi).Power/100) .* obj(oi).MCR;
+               currPower = obj(oi).powerFromMCRPCT(obj(oi).Power, obj(oi).MCR);
                LowestPower(oi) = min(currPower);
                HighestPower(oi) = max(currPower);
+               obj(oi).Lowest_Given_Brake_Power = min(currPower);
+               obj(oi).Highest_Given_Brake_Power = max(currPower);
                
                if ~isempty(obj(oi).SFOC)
-                   currFOC = obj(oi).SFOC .* currPower;
+                   currFOC = obj(oi).FOCFromSFOC(obj(oi).SFOC, currPower);
                    LowestFOCph(oi) = min(currFOC);
+                   obj(oi).MinimumFOC_ph = min(currFOC);
                end
            end
        end
        end
+       
+    end
+    
+    methods(Static)
+       
+       function powerPCT = powerPercentageMCR(power, MCR)
+           
+           powerPCT = (power/MCR) .* 100;
+       end
+       
+       function power = powerFromMCRPCT(powerPCT, MCR)
+           
+           power = powerPCT .* MCR / 100;
+       end
+       
+       function FOC = FOCFromSFOC(SFOC, Power)
+           
+           FOC = SFOC .* Power;
+       end
+       
     end
 end
--- Vessel Descriptions/cVesselEngine/Test/testcVesselEngine.m
+++ Vessel Descriptions/cVesselEngine/Test/testcVesselEngine.m
@@ -11,35 +11,29 @@ methods(Test)
     % Test that method will fit SFOC, Power data to a quadratic function.
     % 1. Test that the object returned by the method will have coefficients
     % calculated using the method of least squares on the input SFOC and
-    % PERCENTMCR*MCR/100, and that all relevant data has been added to the
+    % POWERPCT*MCR/100, and that all relevant data has been added to the
     % object properties.
     
     % 1
     % Input
     szIn = [1, 1e3];
-%     SFOC = abs(randn(szIn)) * 1e2;
     FOC = abs(randn(szIn)) * 1e3;
     a = 1.1; b = 2; c = 0.5;
     Power = FOC.^2.*a + FOC.*b + c;
-%     Power = abs(randn(szIn)) * 1e4;
     input_MCR = max(Power);
     input_SFOC = FOC./Power;
-    input_Power = Power; %(Power/input_MCR) * 100;
+    input_Power = (Power/input_MCR) * 100;
     input_obj = cVesselEngine();
-%     input_obj.MCR = input_MCR;
-%     input_obj.Power = input_Power;
-%     input_obj.SFOC = input_SFOC;
     
     expObj = cVesselEngine();
     expObj.MCR = input_MCR;
-    expObj.Power = (input_Power/input_MCR) * 100;
+    expObj.Power = Power;
     expObj.SFOC = input_SFOC;
-    FOC = input_SFOC.*Power;
-    
-    p = polyfit(FOC, Power, 2);
+
     expObj.X0 = c;
     expObj.X1 = b;
     expObj.X2 = a;
+    expObj.limitsOfData;
     
     % Execute
     actObj = input_obj.fitData2Quadratic(input_MCR, input_SFOC, input_Power);
@@ -47,7 +41,10 @@ methods(Test)
     % Verify
     msgObj = ['Object is expected to have coefficient values assigned to'...
         ' properties after method call.'];
-    testcase.verifyEqual(actObj, expObj, msgObj);
+    reltol = 1e-8;
+    testcase.verifyEqual(actObj.X0, expObj.X0, 'RelTol', reltol, msgObj);
+    testcase.verifyEqual(actObj.X1, expObj.X1, 'RelTol', reltol, msgObj);
+    testcase.verifyEqual(actObj.X2, expObj.X2, 'RelTol', reltol, msgObj);
     
     end
     
@@ -75,16 +72,17 @@ methods(Test)
     expHiPow = max(Power);
     
     % Execute
-    [actLowFOC, actLowPow, actHiPow] = input_obj.limitsOfData;
+    [~, actLowFOC, actLowPow, actHiPow] = input_obj.limitsOfData;
     
     % Verify
     msgLowFOC = 'First output is expected to be the minimum FOC per hour.';
     msgLowPow = 'Second output is expected to be the minimum power value in kW';
     msgHiPow = ['Third output is expected to be the highest value of '...
         'power, in kW.'];
-    testcase.verifyEqual(actLowFOC, expLowFOC, msgLowFOC);
-    testcase.verifyEqual(actLowPow, expLowPow, msgLowPow);
-    testcase.verifyEqual(actHiPow, expHiPow, msgHiPow);
+    reltol = 1e-8;
+    testcase.verifyEqual(actLowFOC, expLowFOC, 'RelTol', reltol, msgLowFOC);
+    testcase.verifyEqual(actLowPow, expLowPow, 'RelTol', reltol, msgLowPow);
+    testcase.verifyEqual(actHiPow, expHiPow, 'RelTol', reltol, msgHiPow);
     
     end
     
