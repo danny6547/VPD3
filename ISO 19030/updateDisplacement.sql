a204bc6ff6a085e5780734e8bc0a7bfbb8c8b012
damcl@hempel.com <damcl@hempel.com>
2017-05-17 12:26:34 +0200

    updateDisplacement.sql: accept displacement if given in raw data

--- ISO 19030/updateDisplacement.sql
+++ ISO 19030/updateDisplacement.sql
@@ -9,15 +9,24 @@ DROP PROCEDURE IF EXISTS updateDisplacement;
 delimiter //
 
 CREATE PROCEDURE updateDisplacement(IMO INT)
-BEGIN
+proc_something:BEGIN
 	
     DECLARE AdjustedTopArea DOUBLE(10, 5);
+    
+    DECLARE displacementGiven BOOLEAN DEFAULT FALSE;
+    DECLARE displacementCalculable BOOLEAN DEFAULT TRUE;
+    
+    SET displacementGiven = (SELECT COUNT(*) FROM tempRawISO WHERE Displacement IS NOT NULL) > 0;
+    
+    IF displacementGiven THEN
+		LEAVE proc_something;
+	END IF;
         
 	IF (SELECT Block_Coefficient FROM Vessels WHERE IMO_Vessel_Number = IMO) IS NULL THEN
 	
 		/* Calculate Displacement based on Hydrostatic Table */
 		UPDATE tempRawISO SET Displacement = (SELECT Displacement FROM Displacement WHERE IMO_Vessel_Number = imo AND
-																			Draft_Actual_Fore = (SELECT Draft_Actual_Fore FROM tempRawISO) AND
+																			Static_Draught_Fore = (SELECT Static_Draught_Fore FROM tempRawISO) AND
 																			Static_Draught_Aft = (SELECT Static_Draught_Aft FROM tempRawISO));
     
     ELSE
