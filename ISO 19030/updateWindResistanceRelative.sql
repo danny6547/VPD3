3b5a42549b9b848a77cde20c4e0feb0b5166efa8
damcl@hempel.com <damcl@hempel.com>
2017-05-23 13:39:55 +0200

    updateWindResistanceRelative: calculate resistance on relative wind at reference

--- ISO 19030/updateWindResistanceRelative.sql
+++ ISO 19030/updateWindResistanceRelative.sql
@@ -14,22 +14,22 @@ BEGIN
 	UPDATE tempRawISO t
 		JOIN (
 			SELECT s1.id, s1.Coefficient
-				FROM (SELECT t.id, Relative_Wind_Direction, Direction, Coefficient, ABS(Relative_Wind_Direction - Direction) AS Diff FROM TempRawISO t 
+				FROM (SELECT t.id, Relative_Wind_Direction_Reference, Direction, Coefficient, ABS(Relative_Wind_Direction_Reference - Direction) AS Diff FROM TempRawISO t 
 					JOIN
 						(SELECT Direction, Coefficient, id AS 'windID' FROM windcoefficientdirection WHERE ModelID = @currModel) w) s1
 							LEFT JOIN
-								(SELECT t.id, Relative_Wind_Direction, Direction, Coefficient, ABS(Relative_Wind_Direction - Direction) AS Diff FROM TempRawISO t 
+								(SELECT t.id, Relative_Wind_Direction_Reference, Direction, Coefficient, ABS(Relative_Wind_Direction_Reference - Direction) AS Diff FROM TempRawISO t 
 									JOIN
 										(SELECT Direction, Coefficient, id AS 'windID' FROM windcoefficientdirection WHERE ModelID = @currModel) w) s2
 											ON s1.id = s2.id AND s1.Diff > s2.Diff
 												WHERE s2.id IS NULL) w
                                                 ON t.id = w.id
-	SET t.Wind_Resistance_Relative =
-		0.5 *
-		Air_Density *
-		POWER(Relative_Wind_Speed, 2) *
-        Transverse_Projected_Area_Current * 
-        w.Coefficient;
+		SET t.Wind_Resistance_Relative =
+				0.5 *
+				Air_Density *
+				POWER(Relative_Wind_Speed_Reference, 2) *
+				Transverse_Projected_Area_Current * 
+				w.Coefficient;
 		/*( SELECT Coefficient FROM WindCoefficientDirection WHERE IMO_Vessel_Number = imo AND Relative_Wind_Direction >= Start_Direction AND Relative_Wind_Direction < End_Direction);*/
 	
 END;
