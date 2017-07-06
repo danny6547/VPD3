bb3ca6b3fd61271f186c3bcf049f6f99ca5bb2f6
damcl@hempel.com <damcl@hempel.com>
2017-07-06 10:37:41 +0200

    createVesselSpeedPowerModel.sql: table for speed, power models

---
+++ ISO 19030/createVesselSpeedPowerModel.sql
@@ -0,0 +1,16 @@
+/* Create table to relate Vessel IMO Numbers with Speed, Power models */
+
+DROP PROCEDURE IF EXISTS createVesselSpeedPowerModel;
+
+delimiter //
+
+CREATE PROCEDURE createVesselSpeedPowerModel()
+
+BEGIN
+
+	CREATE TABLE VesselSpeedPowerModel (id INT PRIMARY KEY AUTO_INCREMENT,
+										IMO_Vessel_Number INT(7),
+                                        Speed_Power_Model INT,
+										CONSTRAINT UNIQUE `UniqueEverything` (`IMO_Vessel_Number`,`Speed_Power_Model`)
+                                        );
+END
