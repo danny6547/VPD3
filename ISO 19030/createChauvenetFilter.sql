38c24add97054d60dd00036cc6bceea002befbb0
damcl@hempel.com <damcl@hempel.com>
2017-07-06 10:11:46 +0200

    createChauvenetFilter.sql: include some columns

--- ISO 19030/createChauvenetFilter.sql
+++ ISO 19030/createChauvenetFilter.sql
@@ -12,16 +12,20 @@ CREATE PROCEDURE createTempChauvenetFilter()
 
 		DROP TABLE IF EXISTS ChauvenetTempFilter;
 
-		CREATE TABLE ChauvenetTempFilter (id INT PRIMARY KEY AUTO_INCREMENT, 
+		CREATE TABLE ChauvenetTempFilter (id INT PRIMARY KEY AUTO_INCREMENT,
+										
 										Speed_Through_Water BOOLEAN, 
 										Delivered_Power BOOLEAN, 
-										Shaft_Revolutions BOOLEAN, 
 										Relative_Wind_Speed BOOLEAN, 
 										Relative_Wind_Direction BOOLEAN, 
 										Speed_Over_Ground BOOLEAN, 
 										Ship_Heading BOOLEAN, 
-										Rudder_Angle BOOLEAN, 
+										Shaft_Revolutions BOOLEAN, 
 										Water_Depth BOOLEAN, 
+										Rudder_Angle BOOLEAN, 
+                                        Air_Temperature BOOLEAN,
+                                        Static_Draught_Fore BOOLEAN,
+                                        Static_Draught_Aft BOOLEAN,
 										Seawater_Temperature BOOLEAN);
 									
 	END;
