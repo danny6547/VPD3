c5bac216257f3bfac0e312f768fee99024405728
damcl@hempel.com <damcl@hempel.com>
2017-08-02 09:27:19 +0200

    removeInvalidRecords.sql: add new conditions for out-of-range values

--- ISO 19030/removeInvalidRecords.sql
+++ ISO 19030/removeInvalidRecords.sql
@@ -1,14 +1,17 @@
-/* Remove rows containing any invalid data (don't worry, I'm gonna rewrite this later.) */
+/* Remove rows containing any invalid data */
 
 
-
+DROP PROCEDURE IF EXISTS removeInvalidRecords;
 
 delimiter //
 
 CREATE PROCEDURE removeInvalidRecords()
+
 BEGIN
 
-	DELETE FROM tempRawISO WHERE Mass_Consumed_Fuel_Oil <= 0;
+	UPDATE tempRawISO SET Mass_Consumed_Fuel_Oil = NULL WHERE Mass_Consumed_Fuel_Oil <= 0;
+    UPDATE tempRawISO SET Water_Depth = NULL WHERE Water_Depth > 1.2E4 OR Water_Depth <= 0;
+    UPDATE tempRawISO SET Relative_Wind_Speed = NULL WHERE Relative_Wind_Speed > 1E3 OR Relative_Wind_Speed <= 0;
     
     
 END;
