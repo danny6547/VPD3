13d646172633adcac86a09a6376f381489e31880
damcl@hempel.com <damcl@hempel.com>
2017-08-02 09:20:30 +0200

    cDB.create: include new files

--- Database Generation/@cDB/cDB.m
+++ Database Generation/@cDB/cDB.m
@@ -21,7 +21,11 @@ classdef cDB < cMySQL
         AdditionalScripts = {...
             ['C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data'...
             '\CMA CGM\Scripts\Insert_data.m']...
+            ['L:\Project\MWB-Fuel efficiency\Hull and propeller performance'...
+            '\Vessels\UASC data\Scripts\Insert_data.m']
             };
+        LoadRaw = true;
+        LoadPerformance = true;
     end
     
     methods
@@ -125,6 +129,11 @@ classdef cDB < cMySQL
                             'updateWindResistanceCorrection.sql'
                             'updateWindResistanceRelative.sql'
                             'validateFrequencies.sql'
+                            'createvesselspeedpowermodel.sql'
+                            'updateDefaultValues.sql'
+                            'updateWindReference.sql'
+                            'knots2mps.sql'
+                            'updateNearestTrim.sql'
                             };
        createFiles1_c = cellfun(@(x) fullfile(topleveldir, ISODir, x), ...
            ISO19030Create_c, 'Uni', 0);
@@ -154,8 +163,17 @@ classdef cDB < cMySQL
            'Documents\SQL\tests\EcoInsight Test Scripts\Accessing Database'...
            '\performanceData.sql']};
        
+       createFiles5_c = {...
+           'createTempMarorkaRaw.sql'
+           'insertFromMarorkaRawIntoRaw.sql'
+           };
+       marorkaDir_ch = ['C:\Users\damcl\OneDrive - Hempel Group\Documents\'...
+           'SQL\tests\EcoInsight Test Scripts\Marorka'];
+       createFiles5_c = cellfun(@(x) fullfile(topleveldir, marorkaDir_ch, x), ...
+           createFiles5_c, 'Uni', 0);
+       
        createFiles_c = [createFiles1_c; createFiles2_c; createFiles3_c;...
-           createFiles4_c];
+           createFiles4_c; createFiles5_c];
        
        % Create procedures for creating tables
        obj.source(createFiles_c);
@@ -180,88 +198,104 @@ classdef cDB < cMySQL
                        'createDNVGLPerformanceData'
                        'createDryDockDates'
                        'createTempChauvenetFilter'
-                       
+                       'createvesselspeedpowermodel'
                        };
        
        % Check there are as many proc as files
        cellfun(@(x) obj.call(x), createProc_c, 'Uni', 0);
        
-       % Insert performance data
-       run(['C:\Users\damcl\OneDrive - Hempel Group\Documents\SQL\tests\'...
-           'EcoInsight Test Scripts\Vessel Data\Load_All_DNVGL_Performance.m']);
+       if obj.LoadPerformance
+           
+           % Insert performance data
+           run(['C:\Users\damcl\OneDrive - Hempel Group\Documents\SQL\tests\'...
+               'EcoInsight Test Scripts\Vessel Data\Load_All_DNVGL_Performance.m']);
+       end
        
        % Insert raw data, bunker delivery note
-       rawFiles_c = {...
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Yang Ming\DNVGL Raw\1 Operational reporting 2017-02-08 11-17.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\TMS\LX7, ZX7\1 Operational reporting 2017-02-08 10-51.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\TMS\CX7, ASPC, BINT, CINT\1 Operational reporting 2017-02-08 11-46.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Teekay\Raw\1 Operational reporting 2016-10-07 11_59_All.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Teekay\Raw\1 Operational reporting 2017-02-08 09-36.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\SETAF-SAGET\1 Operational reporting 2017-02-08 10-49.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Euronav\DNVGL Raw\Sandra, Sara\1 Operational reporting 2017-02-08 10-38.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Euronav\DNVGL Raw\Hakone, Hirado\1 Operational reporting 2017-02-08 10-28.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Euronav\DNVGL Raw\Devon, Hakata\1 Operational reporting 2017-02-08 10-19.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\DFDS Seaways\1 Operational reporting 2017-02-08 10-44.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\CMA CGM\Jules, Litani, Marco\1 Operational reporting 2017-01-03 14_04.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\CMA CGM\Alexander Chopin, Danube, Dalila, Gemini\1 Operational reporting 2017-01-03 13_35.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\CMA CGM\Almaviva, Cassiopeia, Gemini\1 Operational reporting 2017-01-03 12_41.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Anglo-Eastern Ship management\1 Operational reporting 2017-02-08 11-11.csv'
-                    'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\AMCL\1 Operational reporting 2017-02-08 11-15.csv'
-%                     'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Berge Kibo\DNVGL\Raw\1 Operational reporting 2017-03-10 13_58.csv'
-                    };
-       bunkerFiles_c = {...
-           'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Yang Ming\DNVGL Raw\2 Bunker reporting 2017-04-24 08_09.csv'...
-           'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\TMS\2 Bunker reporting 2017-04-24 08_12.csv'...
-           'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Teekay\Raw\2 Bunker reporting 2017-04-24 08_14.csv'...
-           'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\CMA CGM\Raw\2 Bunker reporting 2017-04-24 08_18.csv'...
-           'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\SETAF-SAGET\2 Bunker reporting 2017-04-24 08_21.csv'...
-           'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Euronav\DNVGL Raw\2 Bunker reporting 2017-04-24 08_21.csv'...
-           'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\AMCL\2 Bunker reporting 2017-04-24 08_22.csv'
-           };
-       obj_ves = cVessel();
-       obj_ves.Database = name;
-       obj_ves = obj_ves.insertBunkerDeliveryNoteDNVGL(bunkerFiles_c);
-       obj_ves.loadDNVGLRaw(rawFiles_c);
+       if obj.LoadRaw
+           rawFiles_c = {...
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Yang Ming\DNVGL Raw\1 Operational reporting 2017-02-08 11-17.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\TMS\LX7, ZX7\1 Operational reporting 2017-02-08 10-51.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\TMS\CX7, ASPC, BINT, CINT\1 Operational reporting 2017-02-08 11-46.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Teekay\Raw\1 Operational reporting 2016-10-07 11_59_All.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Teekay\Raw\1 Operational reporting 2017-02-08 09-36.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\SETAF-SAGET\1 Operational reporting 2017-02-08 10-49.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Euronav\DNVGL Raw\Sandra, Sara\1 Operational reporting 2017-02-08 10-38.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Euronav\DNVGL Raw\Hakone, Hirado\1 Operational reporting 2017-02-08 10-28.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Euronav\DNVGL Raw\Devon, Hakata\1 Operational reporting 2017-02-08 10-19.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\DFDS Seaways\1 Operational reporting 2017-02-08 10-44.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\CMA CGM\Jules, Litani, Marco\1 Operational reporting 2017-01-03 14_04.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\CMA CGM\Alexander Chopin, Danube, Dalila, Gemini\1 Operational reporting 2017-01-03 13_35.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\CMA CGM\Almaviva, Cassiopeia, Gemini\1 Operational reporting 2017-01-03 12_41.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Anglo-Eastern Ship management\1 Operational reporting 2017-02-08 11-11.csv'
+                        'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\AMCL\1 Operational reporting 2017-02-08 11-15.csv'
+    %                     'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Berge Kibo\DNVGL\Raw\1 Operational reporting 2017-03-10 13_58.csv'
+                        };
+           bunkerFiles_c = {...
+               'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Yang Ming\DNVGL Raw\2 Bunker reporting 2017-04-24 08_09.csv'...
+               'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\TMS\2 Bunker reporting 2017-04-24 08_12.csv'...
+               'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Teekay\Raw\2 Bunker reporting 2017-04-24 08_14.csv'...
+               'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\CMA CGM\Raw\2 Bunker reporting 2017-04-24 08_18.csv'...
+               'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\SETAF-SAGET\2 Bunker reporting 2017-04-24 08_21.csv'...
+               'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\Euronav\DNVGL Raw\2 Bunker reporting 2017-04-24 08_21.csv'...
+               'C:\Users\damcl\OneDrive - Hempel Group\Documents\Ship Data\AMCL\2 Bunker reporting 2017-04-24 08_22.csv'
+               };
+           obj_ves = cVessel();
+           obj_ves.Database = name;
+           obj_ves = obj_ves.insertBunkerDeliveryNoteDNVGL(bunkerFiles_c);
+           obj_ves.loadDNVGLRaw(rawFiles_c);
+       end
        
        % Load Dry Docking Dates from Files
-       ddFiles = {['L:\Project\MWB-Fuel efficiency\Hull and propeller '...
+       ddFiles = ['L:\Project\MWB-Fuel efficiency\Hull and propeller '...
            'performance\Vessels\CMA CGM\Cart reports 2\Cart reports '...
-           'CMA CGM.xlsx']};
+           'CMA CGM.xlsx'];
        objddd = cVesselDryDockDates();
-       objddd = objddd.readFile(ddFiles);
+       objddd = objddd.readFile(ddFiles, 'dd-mm-yy');
        
-       % Run Additional Scripts
-       for si = 1:numel(scripts_c)
-           
-           % Run script, temporarily adding directory to path if necessary
-           currScript = scripts_c{si};
-           try run(currScript);
-               
-           catch ee
-               
-               if strcmp(ee.identifier, 'MATLAB:UndefinedFunction');
-                   
-                   scriptDir = fileparts(currScript);
-                   addpath(scriptDir);
-                   
-                   try run(currScript);
-                       
-                   catch ee
-                        
-                        rmpath(scriptDir);
-                        throw(ee);
-                   end
-                   
-               else
-                   
-                    throw(ee);
-               end
-           end
-           
-           % Change database of vessel returned by script and insert data
-%            [vess.Database] = deal(name);
+       objddd = cVesselDryDockDates();
+       ddFileEuronav = ['L:\Project\MWB-Fuel efficiency\'...
+           'Hull and propeller performance\Vessels\Euronav\Scripts\'...
+           'DryDockDates.xlsx'];
+       objddd = objddd.readFile(ddFileEuronav, 'dd-mm-yyyy');
+       
+       % Load standard wind coefficients
+       windFile = ['C:\Users\damcl\OneDrive - Hempel Group\Documents\'...
+           'Ship Data\Wind\ISO15016\Insert_ISO15016_Wind_Data.m'];
+       run(windFile);
+       
+%        % Run Additional Scripts
+%        for si = 1:numel(scripts_c)
+%            
+%            % Run script, temporarily adding directory to path if necessary
+%            currScript = scripts_c{si};
+%            try run(currScript);
+%                
+%            catch ee
+%                
+%                if strcmp(ee.identifier, 'MATLAB:UndefinedFunction');
+%                    
+%                    scriptDir = fileparts(currScript);
+%                    addpath(scriptDir);
+%                    
+%                    try run(currScript);
+%                        
+%                    catch ee
+%                         
+%                         rmpath(scriptDir);
+%                         throw(ee);
+%                    end
+%                    
+%                else
+%                    
+%                     throw(ee);
+%                end
+%            end
+%            
+%            % Change database of vessel returned by script and insert data
+% %            [vess.Database] = deal(name);
 %            vess = vess.insert;
-       end
+%        end
        
        end
        
