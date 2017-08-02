455d8a49efe08cbc5cc58918bdd9f204001d98dc
damcl@hempel.com <damcl@hempel.com>
2017-08-02 09:20:51 +0200

    createDisplacementTable.sql: DB displacement table create script

--- ISO 19030/createDisplacementTable.sql
+++ ISO 19030/createDisplacementTable.sql
@@ -1,6 +1,13 @@
-/* Create displacement table for vessel */
+/* Create displacement table, allowing displacements to be found for recorded drafts.
 
-
+ Draft_Aft : metres
+ Draft_Fore : metres
+ Draft_Mean : metres
+ LCF : metres; Length between centre of flotation and mid-ship section
+ TPC : tonnes; tonnes per centimetre of additional mass submerged due to change in draft at this draft
+ Trim : metres; Draft_Aft - Draft_Mean
+ Displacement : m^3; Volume of fluid displaced by the hull
+*/
 
 DROP PROCEDURE IF EXISTS createDisplacement;
 
@@ -11,10 +18,13 @@ CREATE PROCEDURE createDisplacement()
 	BEGIN
 	
 	CREATE TABLE Displacement (id INT PRIMARY KEY AUTO_INCREMENT,
-								 IMO_Vessel_Number INT NOT NULL,
-								 Draft_Aft DOUBLE(10, 5),
-								 Draft_Fore DOUBLE(10, 5),
-								 Trim DOUBLE(10, 5),
-								 Displacement DOUBLE(10, 5));
+								 ModelID INT NOT NULL,
+								 Draft_Aft DOUBLE(5, 3),
+								 Draft_Fore DOUBLE(5, 3),
+								 Draft_Mean DOUBLE(5, 3),
+								 LCF DOUBLE(6, 5),
+								 TPC DOUBLE(7, 3),
+								 Trim DOUBLE(5, 3),
+								 Displacement DOUBLE(20, 5));
 								 
 	END;
