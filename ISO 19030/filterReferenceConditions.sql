1ae3ecf6c8e1238047d8e094af61731b04816ae4
damcl@hempel.com <damcl@hempel.com>
2017-08-02 09:24:16 +0200

    filterReferenceConditions.sql

--- ISO 19030/filterReferenceConditions.sql
+++ ISO 19030/filterReferenceConditions.sql
@@ -24,32 +24,32 @@ BEGIN
 								DepthFormula5 DOUBLE(10, 5), 
 								DepthFormula6 DOUBLE(10, 5));
     
-    INSERT INTO DepthFormula (DateTime_UTC) SELECT DateTime_UTC FROM tempRawISO;
-    UPDATE DepthFormula, tempRawISO SET DepthFormula.Water_Depth = tempRawISO.Water_Depth;
+    INSERT INTO DepthFormula (DateTime_UTC) SELECT DateTime_UTC FROM temprawiso;
+    UPDATE DepthFormula, temprawiso SET DepthFormula.Water_Depth = temprawiso.Water_Depth;
     
     SET ShipBreadth := (SELECT Breadth_Moulded FROM Vessels WHERE IMO_Vessel_Number = imo);
     SET g1 := (SELECT g FROM globalConstants);
     
 	UPDATE DepthFormula d 
-		INNER JOIN tempRawISO t
+		INNER JOIN temprawiso t
 			ON d.DateTime_UTC = t.DateTime_UTC
 				SET 
                 d.DepthFormula5 = 3 * SQRT( ShipBreadth * (t.Static_Draught_Aft + t.Static_Draught_Fore) / 2 ),
                 d.DepthFormula6 = 2.75 * POWER(t.Speed_Through_Water, 2) / g1, 
                 d.Water_Depth = t.Water_Depth;
     
-    UPDATE tempRawISO SET Filter_Reference_Seawater_Temp = TRUE WHERE Seawater_Temperature <= 2;
-    UPDATE tempRawISO SET Filter_Reference_Wind_Speed = TRUE WHERE Relative_Wind_Speed > 7.9;
-    UPDATE tempRawISO SET Filter_Reference_Water_Depth = TRUE WHERE Water_Depth < 3 * SQRT( ShipBreadth * (Static_Draught_Aft + Static_Draught_Fore) / 2 ) 
+    UPDATE temprawiso SET Filter_Reference_Seawater_Temp = TRUE WHERE Seawater_Temperature <= 2;
+    UPDATE temprawiso SET Filter_Reference_Wind_Speed = TRUE WHERE Relative_Wind_Speed > 7.9;
+    UPDATE temprawiso SET Filter_Reference_Water_Depth = TRUE WHERE Water_Depth < 3 * SQRT( ShipBreadth * (Static_Draught_Aft + Static_Draught_Fore) / 2 ) 
 		OR Water_Depth < 2.75 * POWER(Speed_Through_Water, 2) / g1;
-    UPDATE tempRawISO SET Filter_Reference_Rudder_Angle = TRUE WHERE Rudder_Angle > 5;
+    UPDATE temprawiso SET Filter_Reference_Rudder_Angle = TRUE WHERE Rudder_Angle > 5;
     
-	/* DELETE FROM tempRawISO WHERE Seawater_Temperature <= 2; */
+	/* DELETE FROM temprawiso WHERE Seawater_Temperature <= 2; */
     /* 
-	DELETE FROM tempRawISO WHERE Relative_Wind_Speed < 0 OR Relative_Wind_Speed > 7.9;
-	DELETE FROM tempRawISO WHERE Rudder_Angle > 5;
+	DELETE FROM temprawiso WHERE Relative_Wind_Speed < 0 OR Relative_Wind_Speed > 7.9;
+	DELETE FROM temprawiso WHERE Rudder_Angle > 5;
     DELETE d
-		FROM tempRawISO d
+		FROM temprawiso d
         JOIN DepthFormula dd ON d.DateTime_UTC = dd.DateTime_UTC
         WHERE d.Water_Depth < dd.DepthFormula5 OR
 			  d.Water_Depth < dd.DepthFormula6;
