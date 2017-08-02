b5fcaae68de0e18d2d2e35ba6e5d8a0627f856e0
damcl@hempel.com <damcl@hempel.com>
2017-08-02 09:32:28 +0200

    updateExpectedSpeed.sql: make speed loss proportion, not precentage

--- ISO 19030/updateSpeedLoss.sql
+++ ISO 19030/updateSpeedLoss.sql
@@ -1,13 +1,13 @@
 /* Calculate speed loss as a percentage */
 
 
-
+DROP PROCEDURE IF EXISTS updateSpeedLoss;
 
 delimiter //
 
 CREATE PROCEDURE updateSpeedLoss()
 BEGIN
 
-	UPDATE tempRawISO SET Speed_Loss = (100 * (Speed_Through_Water - Expected_Speed_Through_Water) / Expected_Speed_Through_Water);
+	UPDATE tempRawISO SET Speed_Loss = (Speed_Through_Water - Expected_Speed_Through_Water) / Expected_Speed_Through_Water;
 
 END;
