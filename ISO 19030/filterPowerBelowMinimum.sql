c5e2a39be987e2232d96f17b5701310f3cafa89e
damcl@hempel.com <damcl@hempel.com>
2017-08-02 09:23:17 +0200

    filterPowerBelowMinimum.sql: assign into new filter columns

--- ISO 19030/filterPowerBelowMinimum.sql
+++ ISO 19030/filterPowerBelowMinimum.sql
@@ -18,24 +18,29 @@ BEGIN
 		ON t.IMO_Vessel_Number = p.IMO_Vessel_Number AND
 			t.NearestTrim = p.Trim AND
             t.NearestDisplacement = p.Displacement
-		SET t.Filter_SpeedPower_Below = t.Delivered_Power < MIN(p.Power)
+		SET t.Filter_SpeedPower_Below = t.Corrected_Power < MIN(p.Power)
         ; */
     
 	/* Get the minimum power of the power curve corresponding to delivered power, then whether DeliveredPower is less than this value. */
 	UPDATE tempRawISO y JOIN
-		(SELECT q.id, q.BelowMin, q.Delivered_Power FROM
-			(SELECT r.id, r.Delivered_Power, p.Power, r.Delivered_Power < p.Power AS 'BelowMin' FROM tempRawISO r
+		(SELECT q.id, MinPower, MaxPower, MaxSpeed, MinSpeed,
+											IFNULL(q.Speed_Through_Water > MaxSpeed, FALSE) AS SHigh,
+											IFNULL(q.Speed_Through_Water < MinSpeed, FALSE) AS SLow,
+											IFNULL(q.Corrected_Power > MaxPower, FALSE) AS PHigh,
+											IFNULL(q.Corrected_Power < MinPower, FALSE) AS PLow FROM
+		(SELECT r.id, r.Corrected_Power, p.Power, r.NearestDisplacement, r.NearestTrim, r.Speed_Through_Water
+			FROM tempRawISO r
 				JOIN SpeedPower p
 					ON
 					 /* r.IMO_Vessel_Number = p.IMO_Vessel_Number AND */
 					r.NearestTrim = p.Trim AND
 					r.NearestDisplacement = p.Displacement
-                    WHERE p.ModelID IN (SELECT Speed_Power_Model FROM vesselspeedpowermodel WHERE IMO_Vessel_Number = imo)
-                    ) AS q
+					WHERE p.ModelID IN (SELECT Speed_Power_Model FROM vesselspeedpowermodel WHERE IMO_Vessel_Number = imo)
+					) AS q
 		INNER JOIN
-			(SELECT id, Delivered_Power, Power, MIN(Power) AS MinPower
+			(SELECT id, Corrected_Power, Power, MIN(Power) AS MinPower, MAX(Power) AS MaxPower, MIN(Speed) AS MinSpeed, MAX(Speed) AS MaxSpeed
 			FROM
-				(SELECT t.id, t.Delivered_Power, p.Power, t.Delivered_Power < p.Power AS 'BelowMin' FROM tempRawISO t
+				(SELECT t.id, t.Corrected_Power, p.Power, p.Speed FROM tempRawISO t
 					JOIN SpeedPower p
 						ON
 						/* t.IMO_Vessel_Number = p.IMO_Vessel_Number AND */
@@ -44,10 +49,25 @@ BEGIN
 						WHERE p.ModelID IN (SELECT Speed_Power_Model FROM vesselspeedpowermodel WHERE IMO_Vessel_Number = imo)) AS w
 			GROUP BY id) AS e
 		ON
-			q.id = e.id AND
-			q.Power = e.Power
-			GROUP BY q.id) u
-	ON y.id = u.id
-	SET y.Filter_SpeedPower_Below = u.BelowMin
-		;
+		q.id = e.id AND
+		q.Power = e.Power
+		GROUP BY q.id) u
+			ON y.id = u.id
+			SET y.Filter_Speed_Below  = u.SLow,
+				 y.Filter_Speed_Above  = u.SHigh,
+				 y.Filter_Power_Below  = u.PLow,
+				 y.Filter_Power_Above  = u.PHigh
+			;
+    
+    /* Filter variable when STW is NULL */
+	UPDATE tempRawISO SET Filter_Speed_Below  = IFNULL(Filter_Speed_Below, TRUE);
+	UPDATE tempRawISO SET Filter_Speed_Above  = IFNULL(Filter_Speed_Above, TRUE);
+	UPDATE tempRawISO SET Filter_Speed_Below  = TRUE WHERE Speed_Through_Water IS NULL;
+	UPDATE tempRawISO SET Filter_Speed_Above  = TRUE WHERE Speed_Through_Water IS NULL;
+    
+	UPDATE tempRawISO SET Filter_Power_Below  = IFNULL(Filter_Power_Below, TRUE);
+	UPDATE tempRawISO SET Filter_Power_Above  = IFNULL(Filter_Power_Above, TRUE);
+	UPDATE tempRawISO SET Filter_Power_Below  = TRUE WHERE Corrected_Power IS NULL;
+	UPDATE tempRawISO SET Filter_Power_Above  = TRUE WHERE Corrected_Power IS NULL;
+            
 END
