2579d46409dca9509fdedce7410306dfb8b4500c
damcl@hempel.com <damcl@hempel.com>
2017-08-22 08:35:12 +0200

    createWindCoefficients: default value

--- ISO 19030/createWindCoefficients.sql
+++ ISO 19030/createWindCoefficients.sql
@@ -10,7 +10,7 @@ BEGIN
 create table windcoefficientdirection (id INT PRIMARY KEY AUTO_INCREMENT, 
 																ModelID INT NOT NULL, 
                                                                 Direction DOUBLE(4, 1) NOT NULL DEFAULT 0,
-                                                                Coefficient DOUBLE(9, 8) NOT NULL,
+                                                                Coefficient DOUBLE(9, 8) NOT NULL DEFAULT 0,
                                                                 Name TEXT,
                                                                 CONSTRAINT UniModelDirs UNIQUE (ModelID, Direction));
                                                                 
