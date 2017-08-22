d7cb77b2b4385f0f7e455d5419eab61c141c6a03
damcl@hempel.com <damcl@hempel.com>
2017-08-22 08:30:46 +0200

    createDisplacementTable: add constraints, remove drafts

--- ISO 19030/createDisplacementTable.sql
+++ ISO 19030/createDisplacementTable.sql
@@ -19,12 +19,13 @@ CREATE PROCEDURE createDisplacement()
 	
 	CREATE TABLE Displacement (id INT PRIMARY KEY AUTO_INCREMENT,
 								 ModelID INT NOT NULL,
-								 Draft_Aft DOUBLE(5, 3),
-								 Draft_Fore DOUBLE(5, 3),
 								 Draft_Mean DOUBLE(5, 3),
 								 LCF DOUBLE(6, 5),
 								 TPC DOUBLE(7, 3),
 								 Trim DOUBLE(5, 3),
-								 Displacement DOUBLE(20, 5));
+								 Displacement DOUBLE(20, 5),
+                                 CONSTRAINT UniqueModelDraft UNIQUE(ModelID, Draft_Mean),
+                                 CONSTRAINT UniqueModelDraftTrim UNIQUE(ModelID, Draft_Mean, Trim)
+                                 );
 								 
 	END;
