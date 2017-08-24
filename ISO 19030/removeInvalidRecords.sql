211eb832d30a5900dad60236c3639fcb0126d962
damcl@hempel.com <damcl@hempel.com>
2017-08-24 13:31:16 +0200

    removeInvalidRecords.sql: fix bug where zero speed was changed to NULL

--- ISO 19030/removeInvalidRecords.sql
+++ ISO 19030/removeInvalidRecords.sql
@@ -11,7 +11,7 @@ BEGIN
 
 	UPDATE tempRawISO SET Mass_Consumed_Fuel_Oil = NULL WHERE Mass_Consumed_Fuel_Oil <= 0;
     UPDATE tempRawISO SET Water_Depth = NULL WHERE Water_Depth > 1.2E4 OR Water_Depth <= 0;
-    UPDATE tempRawISO SET Relative_Wind_Speed = NULL WHERE Relative_Wind_Speed > 1E3 OR Relative_Wind_Speed <= 0;
+    UPDATE tempRawISO SET Relative_Wind_Speed = NULL WHERE Relative_Wind_Speed > 1E3 OR Relative_Wind_Speed < 0;
     
     
 END;
