0728f6f066ade8bcdb2869d8a94ae74109ea373b
damcl@hempel.com <damcl@hempel.com>
2018-01-22 08:19:25 +0100

    cV: remove redundant methods

--- Vessel Descriptions/cVessel/Code/@cVessel/iter.m
+++
@@ -1,9 +0,0 @@
-function [obj, currIter] = iter(obj)
-%iter Iterate over each element in object array
-%   Detailed explanation goes here
-
-currIter = obj(1).CurrIter;
-obj = obj.checkFinishIter(currIter);
-
-nextIter = currIter + 1;
-[obj.CurrIter] = deal(nextIter);
