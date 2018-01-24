3425fe41ec2c5b18332b4ce797807b7a0d5551ea
damcl@hempel.com <damcl@hempel.com>
2018-01-24 10:36:08 +0100

    cVP: class for storing vessel particulars

---
+++ Vessel Descriptions/cVesselParticulars/@cVesselParticulars/cVesselParticulars.m
@@ -0,0 +1,36 @@
+classdef cVesselParticulars < cMySQL
+    %CVESSELPARTICULARS Data relating to vessel particulars
+    %   Detailed explanation goes here
+    
+    properties
+        
+        IMO_Vessel_Number double = [];
+        Name char = '';
+        Owner char = '';
+        Class = [];
+        LBP = [];
+        Transverse_Projected_Area_Design = [];
+        Length_Overall = [];
+        Breadth_Moulded = [];
+        Draft_Design = [];
+        Anemometer_Height = [];
+    end
+    
+    properties(Hidden)
+        
+        Wind_Model_ID;
+    end
+    
+    methods
+    
+       function obj = cVesselParticulars()
+       
+       end
+       
+       function obj = insertIntoTable(obj)
+           
+           tab = 'Vessels';
+           insertIntoTable@cMySQL(obj, tab);
+       end
+    end
+end
