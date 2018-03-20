6ac7e40fda45b0d63c6ff337e3eb898ad1f2b302
damcl@hempel.com <damcl@hempel.com>
2018-03-20 09:46:38 +0100

    cV.activityFromSeaWeb: update to reflect apparent recent changes in seaweb downloads

--- Vessel Descriptions/cVessel/Code/@cVessel/activityFromSeaWeb.m
+++ Vessel Descriptions/cVessel/Code/@cVessel/activityFromSeaWeb.m
@@ -18,33 +18,61 @@ if any(~validFile)
 end
 
 % Iterate over files and append to table
-empty_c = cell(1, 12);
-fileVars_c = {'Ship', 'IMO_LR_IHS_No_',...
-    'Port_of_Call', 'PortOfCall_URL', 'Country_of_Call', 'Arrival_Date',...
-    'Sailed_Date', 'Hours_in_Port', 'Arrival_Date_1', 'Ship_Name',...
+% fileVars_c = {'Ship', 'IMO_LR_IHS_No_',...
+%     'Port_of_Call', 'PortOfCall_URL', 'Country_of_Call', 'Arrival_Date',...
+%     'Sailed_Date', 'Hours_in_Port', 'Arrival_Date_1', 'Ship_Name',...
+%     'Vessel_Type', 'Operator'};
+fileVarsXLS_c = {'IMO_LR_IHS_No_', 'LRN01', 'Ship_Name', 'Port_of_Call', ...
+    'Country_of_Call', 'Arrival_Date',...
+    'Sailed_Date', 'Hours_in_Port', 'Arrival_Date_1', ...
     'Vessel_Type', 'Operator'};
-ihs_tbl = table(empty_c{:}, 'VariableNames', fileVars_c);
+empty_c = cell(1, numel(fileVarsXLS_c));
+% ihs_tbl = table(empty_c{:}, 'VariableNames', fileVars_c);
+ihs_tbl = table(empty_c{:}, 'VariableNames', fileVarsXLS_c);
 
 for fi = 1:numel(filename)
 
     % Parse File
     currFile = filename{fi};
-    t = readtable(currFile, 'ReadVariableNames', false, 'HeaderLines', 1,...
+%     t = readtable(currFile, 'ReadVariableNames', false, 'HeaderLines', 1,...
+%         'DatetimeType', 'text');
+    t = readtable(currFile, 'ReadVariableNames', true, ...
         'DatetimeType', 'text');
-
+    
     % Filter missing times from table
-    t.Properties.VariableNames = fileVars_c;
-    t(cellfun(@isempty, t.Arrival_Date) | cellfun(@isempty, t.Sailed_Date), :)= [];
-
+%     t.Properties.VariableNames = fileVars_c;
+    t.Properties.VariableNames = fileVarsXLS_c;
+    if iscell(t.Arrival_Date)
+        
+        t(cellfun(@isempty, t.Arrival_Date) | cellfun(@isempty, t.Sailed_Date), :)= [];
+    else
+        
+        t(isnan(t.Arrival_Date) | isnan(t.Sailed_Date), :)= [];
+    end
+    
     % Append to existing table
     ihs_tbl = [ihs_tbl; t];
 end
 
 % Calculate Idle time as difference of arrival and departures
-ihs_tbl.Arrival_Datenum = datenum(ihs_tbl.Arrival_Date, 'mm/dd/yyyy HH:MM:SS PM');
-ihs_tbl.Sailed_Datenum = datenum(ihs_tbl.Sailed_Date, 'mm/dd/yyyy HH:MM:SS PM');
-ihs_tbl.Idle_Time = ihs_tbl.Sailed_Datenum - ihs_tbl.Arrival_Datenum;
+% ihs_tbl(1, :) = [];
+if iscell(ihs_tbl.Arrival_Date)
     
+    ihs_tbl.Arrival_Datenum = datenum(ihs_tbl.Arrival_Date, 'mm/dd/yyyy HH:MM:SS PM');
+    ihs_tbl.Sailed_Datenum = datenum(ihs_tbl.Sailed_Date, 'mm/dd/yyyy HH:MM:SS PM');
+else
+    
+    x2m = @(xld) xld + datenum('01-01-1900', 'dd-mm-yyyy') - 2;
+    ihs_tbl.Arrival_Datenum = x2m(ihs_tbl.Arrival_Date);
+    ihs_tbl.Sailed_Datenum = x2m(ihs_tbl.Sailed_Date);
+end
+ihs_tbl.Idle_Time = ihs_tbl.Sailed_Datenum - ihs_tbl.Arrival_Datenum;
+
+if iscell(ihs_tbl.IMO_LR_IHS_No_)
+    
+    ihs_tbl.IMO_LR_IHS_No_ = cellfun(@str2double, ihs_tbl.IMO_LR_IHS_No_);
+end
+
 % Activity defined as ratio between total idle time and total time
 % while ~obj.iterFinished
 %     
@@ -55,7 +83,7 @@ ihs_tbl.Idle_Time = ihs_tbl.Sailed_Datenum - ihs_tbl.Arrival_Datenum;
 %     end
 %     
 % for ii = 1:numel(obj)
-    
+
 while obj.iterateDD
     
     % 
