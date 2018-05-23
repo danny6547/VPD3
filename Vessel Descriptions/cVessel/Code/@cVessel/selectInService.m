8009d052e154ed38c8c9c9ce603bc307117ce548
damcl@hempel.com <damcl@hempel.com>
2018-05-23 12:49:00 +0200

    cV.selectInService: iterate OBJ
    --HG--
    branch : Delegate DB Spec

--- Vessel Descriptions/cVessel/Code/@cVessel/selectInService.m
+++ Vessel Descriptions/cVessel/Code/@cVessel/selectInService.m
@@ -2,24 +2,25 @@ function [ obj ] = selectInService(obj, varargin)
 %selectInService Select InService data from database
 %   Detailed explanation goes here
 
-% Input
-cols = '*';
-if nargin > 1 && ~isempty(varargin{1})
+for oi = 1:numel(obj)
+
+    % Input
+    cols = {'Timestamp', obj(oi).Variable};
+    if nargin > 1 && ~isempty(varargin{1})
+        
+        cols = varargin{1};
+        cols = validateCellStr(cols);
+        cols = obj(oi).SQL.validateColumnNames(cols);
+    end
     
-    cols = varargin{1};
-    cols = validateCellStr(cols);
-    cols = obj.SQL.validateColumnNames(cols);
-end
-
-% Data identified by Vessel Configuration
-vcid = [obj.Configuration.Vessel_Configuration_Id];
-vcid_ch = num2str(vcid);
-
-% Select data and assign
-tab = 'CalculatedData c JOIN RawData r ON c.Raw_Data_Id = r.Raw_Data_Id';
-% cols = '*';
-where = ['c.Vessel_Configuration_Id = ', vcid_ch];
-[~, tbl] = obj.SQL.select(tab, cols, where);
-obj.InService = tbl;
-
+    % Data identified by Vessel Configuration
+    vcid = [obj(oi).Configuration.Vessel_Configuration_Id];
+    vcid_ch = num2str(vcid);
+    
+    % Select data and assign
+    tab = 'CalculatedData c JOIN RawData r ON c.Raw_Data_Id = r.Raw_Data_Id';
+    % cols = '*';
+    where = ['c.Vessel_Configuration_Id = ', vcid_ch];
+    [~, tbl] = obj(oi).SQL.select(tab, cols, where);
+    obj(oi).InService = tbl;
 end
