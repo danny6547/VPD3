2a3bc84415b31f0fd1afaba87b851da43fe9257c
damcl@hempel.com <damcl@hempel.com>
2018-08-02 13:48:25 +0200

    Update spc template
    --HG--
    branch : Delegate DB Spec

--- Vessel Data/Speed_Power_Coefficients_Template.m
+++ Vessel Data/Speed_Power_Coefficients_Template.m
@@ -2,7 +2,6 @@
 % will create a cVessel object in variable VESS and insert the data into 
 % the database.
 
-% Assign Speed, Power
 sp = cVesselSpeedPower();
 
 knots2mps = 0.514444444;
@@ -13,7 +12,7 @@ sp(1).Speed = design(:, 1) * knots2mps; % m/s
 sp(1).Power = design(:, 2) * bhp2Kw; % kW
 sp(1).Trim = [];% Positive by the bow, negative by the stern
 sp(1).Displacement = []; % tonnes of seawater displaced at given seawater density
-sp(1).FluidDensity = []; % tn / m^3, optional, default values is 1025
+sp(1).FluidDensity = []; % kg / m^3, optional, default values is 1025
 sp(1).Name = 'Design';
 
 ballast = [...
@@ -24,3 +23,5 @@ sp(2).Trim = [];
 sp(2).Displacement = [];
 sp(2).FluidDensity = [];
 sp(2).Name = 'Ballast';
+
+sp.print;
