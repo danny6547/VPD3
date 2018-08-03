408c1dd6f0531a11a2288c331132105b6b4f9a57
damcl@hempel.com <damcl@hempel.com>
2018-08-03 13:31:45 +0200

    cMTH.output: includes optional call to plot filter UI
    --HG--
    branch : Delegate DB Spec

--- cMTurkHIT/@cMTurkHIT/output.m
+++ cMTurkHIT/@cMTurkHIT/output.m
@@ -1,9 +1,24 @@
-function [obj, sql] = output(obj)
+function [obj, sql] = output(obj, varargin)
 %output Operations required to process output from mTurk API
 %   Detailed explanation goes here
 
+% Input
+ui = true;
+if nargin > 1
+    
+    ui = varargin{1};
+    validateattributes(ui, {'logical'}, {'scalar'}, ...
+        'cMTurkHIT.output', 'ui', 2);
+end
+
 % Parse results
 obj = obj.results();
+
+% Filter
+if ui
+    
+    obj = obj.plotFilter;
+end
 
 % Write SQL insert statement
 sql = obj.printSQL('Insert.sql');
