a42af6d559f12a43581af18318afe49b7e2afee4
martu1 <none@none>
2018-08-28 07:43:05 +0200

    Control reading of both files through inputs
    --HG--
    branch : Delegate DB Spec

--- DNVGL Raw/SHAPE Report from DNVGL/SHAPE_Report_From_DNVGL.m
+++ DNVGL Raw/SHAPE Report from DNVGL/SHAPE_Report_From_DNVGL.m
@@ -1,21 +1,24 @@
 %% Input
 
-imo = 9732565;
+imo = 9290086;
 activityFileName = '';
-obj = cVessel('SavedConnection', 'hullperformance');
-filename = '\\hempelgroup.sharepoint.com@SSL\DavWWWRoot\sites\HullPerformanceManagementTeam\Vessel Library\Euronav\Time series data\Aquitaine\EI2123 Speed deviation - Single vessel timeline.xlsx';
-obj = obj.loadDNVGLPerformance(filename, imo);
+filename = 'C:\Users\martu\OneDrive - Hempel Group\Documents\TEMP\TI Hellas\TI Hellas.xlsx';
+readfile = true;
 
 %% Create Report
 
 % Get Vessel from SHAPE
+obj = cVessel('SavedConnection', 'hullperformance');
 obj.IMO = imo;
 
 % Connect to local DNVGL DB
 objdnv = cVessel('SavedConnection', 'dnvgl');
+if readfile
+    objdnv = objdnv.loadDNVGLPerformance(filename, imo);
+end
 
 % Read data from local DNVGL DB into object
-imo_ch = num2str(9732565);
+imo_ch = num2str(imo);
 [~, dnvtbl] = objdnv.SQL.select('PerformanceData', ...
         {'date_format(DateTime_UTC, ''%Y-%m-%d %T.%f'') AS Timestamp',...
                             'Performance_Index', 'Speed_Index'},...
@@ -28,8 +31,12 @@ obj = obj.inServicePerformance;
 obj = obj.plotPerformanceData;
 [obj, avgQuarter_st] = obj.movingAverage(365.25/4, true, false, false);
 dd = datetime(obj.DryDock(1).End_Date, 'InputFormat', 'yyyy-mm-dd');
-[tbl5, idleDD_tbl, ~, speed_tbl] = obj.activityFromVesselTrackerXLSX(activityFileName, dd);
-stw = speed_tbl.speed;
-dates = speed_tbl.datetime;
-obj.speedHistogram(dates, stw);
-obj.plotSTW(dates, stw);
+if isempty(activityFileName)
+    
+    [tbl5, idleDD_tbl, ~, speed_tbl] = ...
+        obj.activityFromVesselTrackerXLSX(activityFileName, dd);
+    stw = speed_tbl.speed;
+    dates = speed_tbl.datetime;
+    obj.speedHistogram(dates, stw);
+    obj.plotSTW(dates, stw);
+end
