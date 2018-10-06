4f6229ec9da911ff3fdb426ea049c96e82e4b5c6
damcl <none@none>
2018-10-06 10:18:46 +0200

    create cVA, again
    --HG--
    branch : Delegate DB Spec

---
+++ Vessel Descriptions/cVesselAnalysis/@cVesselAnalysis/cVesselAnalysis.m
@@ -0,0 +1,16 @@
+classdef cVesselAnalysis
+    %CVESSELANALYSIS Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        
+        Vessel_Configuration_Id;
+    end
+    
+    methods
+    
+       function obj = cVesselAnalysis()
+    
+       end
+    end
+end
